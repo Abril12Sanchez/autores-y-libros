4a47389b06f2d7b1259de75cf16b4334e3cfe22d
Abril Sánchez <137373616+Abril12Sanchez@users.noreply.github.com>
2025-04-07 22:06:49 -0600

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,6 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-      apiKey: "AIzaSyCYaDbZX6dgyuvpAQ-DldDDQndf43o47Ic",
-      authDomain: "authds02.firebaseapp.com",
-      projectId: "authds02",
-      storageBucket: "authds02.firebasestorage.app",
-      messagingSenderId: "720542749497",
-      appId: "1:720542749497:web:3cd9e82641637f122acee8"
+      //se eliminaron credenciales
       }      
 };
