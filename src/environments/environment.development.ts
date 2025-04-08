f266af50bfdc2e685404fbc87ad8abb39e33c28c
Abril12Sanchez <doloresabrilsanchez@gmail.com>
2025-04-07 22:23:45 -0600

    crede

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-      //se eliminaron credenciales
+      apiKey: "AIzaSyCYaDbZX6dgyuvpAQ-DldDDQndf43o47Ic",
+      authDomain: "authds02.firebaseapp.com",
+      projectId: "authds02",
+      storageBucket: "authds02.firebasestorage.app",
+      messagingSenderId: "720542749497",
+      appId: "1:720542749497:web:3cd9e82641637f122acee8"
       }      
 };
